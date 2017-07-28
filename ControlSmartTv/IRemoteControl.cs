f776cf3568bcda85bbb408ee7dc1525e4690ac0e
Teemu <Teemu@DESKTOP-PK94DMM>
2017-07-28 11:45:44 +0100

    Using mainclass / interface / tv class

--- ControlSmartTv/IRemoteControl.cs
+++ ControlSmartTv/IRemoteControl.cs
@@ -11,6 +11,7 @@ namespace ControlSmartTv {
         int Channel { get; set; } 
         
         void ChangeChannel(ICollection<KeyValuePair<int, string>> programs);
+    
 
         void ChannelList(ICollection<KeyValuePair<int, string>> programs);
         
